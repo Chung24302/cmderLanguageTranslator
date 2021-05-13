1f9b852a1c2e7a71527cf63ae0f5c57d06e87863
Chung24302 <nhcue10524202@gapp.nthu.edu.tw>
2021-05-13 15:50:06 +0800

    add path input

--- replace.py
+++ replace.py
@@ -1,13 +1,16 @@
 from opencc import OpenCC
+import os
 cc = OpenCC('s2twp')
-
-with open(r'C:\cmder\vendor\conemu-maximus5\ConEmu\ConEmu.l10n', 'r', encoding = 'utf-8') as fr:
+path = input('ConEmu/Cmder Language Path: ')+'\\'
+with open(path + 'ConEmu.l10n', 'r', encoding = 'utf-8') as fr:
     texts = fr.readlines()
     fr.close()
 
+os.rename(path+'ConEmu.l10n',path+'ConEmu-BK.l10n')
+
 texts[6] = texts[6].replace('简体中文', '繁體中文')
 
-with open('tmp.txt','a+' , encoding = 'utf-8') as fw:
+with open(path+'ConEmu.l10n','a+' , encoding = 'utf-8') as fw:
     for line in texts:
         if '"zh"' in line:
             fw.write(cc.convert(line))
